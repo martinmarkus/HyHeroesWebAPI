d5975f17e080fee2c64a62fe297709783d1c40be
Martin Márkus <martinmarkus0@gmail.com>
2021-02-07 14:49:23 +0100

    Add ZipCode annotations

--- Presentation/DTOs/ZipCode.cs
+++ Presentation/DTOs/ZipCode.cs
@@ -1,9 +1,13 @@
-﻿namespace HyHeroesWebAPI.Presentation.DTOs
+﻿using Newtonsoft.Json;
+
+namespace HyHeroesWebAPI.Presentation.DTOs
 {
     public class ZipCode
     {
+        [JsonProperty("zip")]
         public int Zip { get; set; }
 
+        [JsonProperty("city")]
         public string City { get; set; }
     }
 }
